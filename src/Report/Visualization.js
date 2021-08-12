93223c78c5ebcee6477546e9bb84bc06b8b0acbd
husam7 <40503939+husam7@users.noreply.github.com>
2021-08-13 00:39:59 +0300

    number string in Arabic

--- src/Report/Visualization.js
+++ src/Report/Visualization.js
@@ -1,6 +1,5 @@
-
-import store from '@/store'
-import { nArabicWords } from './nArabicWords.js';
+import store from "@/store";
+import { nArabicWords } from "./nArabicWords.js";
 
 let toFixed = store.getters.settings.ToFixed;
 
@@ -8,22 +7,10 @@ export default function Visualization(Data, Html, Type) {
   let ConvetNumberToArabicWords = nArabicWords;
   let formatDate = formatDateX;
   let TimeConvert = TimeConvertX;
+  let ToFixed = ToFixedX;
   //console.log(ConvetNumberToArabicWords)
   Object.keys(Data).forEach(key => {
     Html = Html.replaceAll("{{" + key + "}}", Data[key]);
-  });
-  Object.keys(Data).forEach(key => {
-    let Equation = Html.slice(
-      Html.search('{#'),
-      Html.indexOf('/}', Html.search('{#')) + 2
-    );
-    if (Equation != "" && Equation.search("{{") <= -1) {
-      let evalV = Equation.replace("{#", "")
-      evalV = evalV.replace("/}", "")
-      console.log('evalV  ' + evalV)
-
-      Html = Html.replace(Equation, eval(evalV));
-    }
   });
 
   if (Data.InventoryMovements) {
@@ -33,32 +20,70 @@ export default function Visualization(Data, Html, Type) {
     );
     let tabelInventoryMovements = "";
     Data.InventoryMovements.forEach(element => {
-      let row = res
+      let row = res;
       Object.keys(element).forEach(key => {
         // replace All keys to values
         row = row.replaceAll("{{item." + key + "}}", element[key]);
         // find if have Equation {#  /}
-        let Equation = row.slice(
-          row.search('{#'),
-          row.indexOf('/}', row.search('{#')) + 2
-        );
-        if (Equation != "" && Equation.search("{{") <= -1) {
-          let evalV = Equation.replace("{#", "")
-          evalV = evalV.replace("/}", "")
-          row = row.replace(Equation, eval(evalV));
+        var searchKeyword = "{#";
+        var startingIndices = [];
+        var indexOccurence = row.indexOf(searchKeyword, 0);
+        while (indexOccurence >= 0) {
+          startingIndices.push(indexOccurence);
+          let Equation = row.slice(
+            row.search("{#"),
+            row.indexOf("/}", row.search("{#")) + 2
+          );
+          if (Equation != "" && Equation.search("{{") <= -1) {
+            let evalV = Equation.replace("{#", "");
+            evalV = evalV.replace("/}", "");
+            console.log("evalV  " + evalV);
+
+            row = row.replace(Equation, eval(evalV));
+          }
+          indexOccurence = row.indexOf(searchKeyword, indexOccurence + 1);
         }
       });
-      tabelInventoryMovements += row
+      tabelInventoryMovements += row;
     });
-    Html = Html.replaceAll(Html.slice(
-      Html.search('<tr id="InventoryMovements"'),
-      Html.indexOf("</tr>", Html.search('<tr id="InventoryMovements"')) + 5
-    ), tabelInventoryMovements);
+    Html = Html.replaceAll(
+      Html.slice(
+        Html.search('<tr id="InventoryMovements"'),
+        Html.indexOf("</tr>", Html.search('<tr id="InventoryMovements"')) + 5
+      ),
+      tabelInventoryMovements
+    );
   }
+  var searchKeyword = "{#";
+
+  var startingIndices = [];
+
+  var indexOccurence = Html.indexOf(searchKeyword, 0);
+
+  while (indexOccurence >= 0) {
+    startingIndices.push(indexOccurence);
+    let Equation = Html.slice(
+      Html.search("{#"),
+      Html.indexOf("/}", Html.search("{#")) + 2
+    );
+    if (Equation != "" && Equation.search("{{") <= -1) {
+      let evalV = Equation.replace("{#", "");
+      evalV = evalV.replace("/}", "");
+      console.log("evalV  " + evalV);
+
+      Html = Html.replace(Equation, eval(evalV));
+    }
+    indexOccurence = Html.indexOf(searchKeyword, indexOccurence + 1);
+  }
+  console.log("startingIndices  ", startingIndices);
+
   if (Type == "Set") {
     //  Html += nArabicWords(2000)
     return Html;
   }
+}
+export function ToFixedX(num) {
+  return num.toFixed(toFixed);
 }
 
 export function formatDateX(date) {
