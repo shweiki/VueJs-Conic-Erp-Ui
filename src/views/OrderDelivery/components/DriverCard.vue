6d584cb899ef4ee4b48506ed115ffae42e6c2f9b
husam7 <40503939+husam7@users.noreply.github.com>
2021-09-10 17:26:27 +0300

    OrderDelivery

--- src/views/OrderDelivery/components/DriverCard.vue
+++ src/views/OrderDelivery/components/DriverCard.vue
@@ -1,83 +1,173 @@
 <template>
-  <section class="overview-block-ptb">
-    <div class="container">
+ <section class="overview-block-ptb">
+  <el-row type="flex">
+      <el-col :span="4">
+        <el-input
+          v-model="listQuery.Any"
+          placeholder="Search By Any  Name Or Id"
+          class="filter-item"
+          @keyup.enter.native="handleFilter"
+        />
+      </el-col>
+
+      <el-col :span="3">
+        <Sort-Options
+          :Value="listQuery.Sort"
+          @Set="
+            (v) => {
+              listQuery.Sort = v;
+              handleFilter();
+            }
+          "
+        />
+      </el-col>
+      <el-col :span="10">
+        <Radio-Oprations
+          TableName="OrderDelivery"
+          @Set="
+            (v) => {
+              listQuery.Status = v;
+              handleFilter();
+            }
+          "
+        />
+      </el-col>
+      <el-col :span="6">
+        <el-button
+          v-waves
+          class="filter-item"
+          type="primary"
+          icon="el-icon-search"
+          @click="handleFilter"
+        >
+          Search
+        </el-button>
+      </el-col>
+    </el-row>
+    <div v-loading="listLoading" class="container">
       <div class="row">
-        <el-row :gutter="24" class="panel-group" style="margin: 20px 2px">
-          <div v-for="(option, index) in DeliveryData" :key="index">
-            <el-col :span="24" class="card-panel-col">
+        <el-row :gutter="20" class="panel-group" style="margin: 20px 2px">
+          <div v-for="(option, index) in list" :key="index">
+            <el-col :xs="24" :sm="12" :md="8" :lg="6" class="card-panel-col">
               <div class="card-panel">
-                <el-col>
-                  <el-row>
+                <el-row :gutter="20">
+                  <el-col :span="8">
+                    <div class="card-panel-icon-wrapper icon-people">
+                      <Status-Icon
+                        class="card-panel-icon"
+                        :Status="option.Status"
+                        TableName="OrderDelivery"
+                      />
+                    </div>
+                  </el-col>
+                  <el-col :span="4">
+                    <div class="card-panel-description">
+                      <div class="card-panel-id">{{ option.Id }}</div>
+                    </div>
+                  </el-col>
+                  <el-col :span="12">
                     <div class="card-panel-description">
                       <div class="card-panel-time">
                         {{ option.FakeDate | parseTime("{m}-{d} {h}:{i}") }}
                       </div>
                     </div>
-                  </el-row>
-                  <el-row>
+                  </el-col>
+                </el-row>
+
+                <el-row :gutter="24">
+                  <el-col :span="12">
+                    <div class="card-panel-description">
+                      <div class="card-panel-phone">
+                         <el-tag size="small" type="success">
+                            <a :href="`tel:+${ option.PhoneNumber }`"> {{ option.PhoneNumber }} </a>
+                         </el-tag> 
+                       </div>
+                    </div>
+                  </el-col>
+                  <el-col :span="12">
                     <div class="card-panel-description">
                       <div class="card-panel-name">{{ option.Name }}</div>
                     </div>
-                  </el-row>
-                  <el-row>
+                  </el-col>
+                </el-row>
+                <el-row :gutter="24">
+                  <el-col :span="12">
+                    <div class="card-panel-description">
+                      <div class="card-panel-id">
+                        {{ option.TotalPrice.toFixed($store.getters.settings.ToFixed) }}
+                        JOD
+                      </div>
+                    </div>
+                  </el-col>
+                  <el-col :span="12">
                     <div class="card-panel-description">
                       <div class="card-panel-name">{{ option.Region }}</div>
                     </div>
-                  </el-row>
-                  <el-row>
-                    <div class="card-panel-description">
-                      <div class="card-panel-name">{{ option.TotalPrice }}</div>
-                    </div>
-                  </el-row>
-                  <el-row>
-                    <order-details
-                      :Id="option.Id"
-                      :Status="option.Status"
-                      :Name="option.Name"
-                      :PhoneNumber="option.PhoneNumber"
-                      :TotalPill="option.TotalPill"
-                      :TotalPrice="option.TotalPrice"
-                      :Description="option.Description"
-                      :FakeDate="option.FakeDate"
-                      :Region="option.Region"
-                      :DeliveryPrice="option.DeliveryPrice"
-                      :Driver="option.Driver"
-                      :Content="option.Content"
-                    />
-                  </el-row>
-                </el-col>
-                <el-col></el-col>
-                <el-col></el-col>
+                  </el-col>
+                </el-row>
+                <el-row :gutter="24">
+                    <order-details-mobile :Temp="option" />
+                </el-row>
+                <!-- <el-row v-if="$store.getters.device === 'mobile'" :gutter="24">
+                    <driver-to-order-mobile :Temp="option" @Done="handleFilter()" />
+                    <order-details-mobile :Temp="option" />
+                </el-row> -->
               </div>
             </el-col>
           </div>
         </el-row>
       </div>
     </div>
+      <pagination
+      v-show="Totals.Rows > 0"
+      :total="Totals.Rows"
+      :page.sync="listQuery.Page"
+      :limit.sync="listQuery.limit"
+      @pagination="getdata"
+    />
   </section>
 </template>
 <script>
-import OrderDetails from "./OrderDetails.vue";
-import DriverToOrder from "./DriverToOrder.vue";
+import OrderDetailsMobile from "./OrderDetailsMobile.vue";
 import { GetDriverOrder } from "@/api/OrderDelivery";
 import { mapGetters } from "vuex";
+import RadioOprations from "@/components/Oprationsys/RadioOprations.vue";
+import StatusIcon from "@/components/Oprationsys/StatusIcon.vue";
+import waves from "@/directive/waves";
+import Pagination from "@/components/Pagination";
+import SortOptions from "@/components/SortOptions";
 export default {
   name: "DeliveryCards",
   components: {
-    OrderDetails,
-    DriverToOrder,
+    OrderDetailsMobile,
+    RadioOprations,
+    StatusIcon,
+    Pagination,
+    SortOptions
+
   },
+  directives: { waves },
   data() {
     return {
-      DeliveryData: [],
+      //DeliveryData: [],
+      list: [],
+      Totals: { Rows: 0 },
       user: {},
+      listLoading: false,
+        listQuery: {
+        Page: 1,
+        Any: "",
+        limit: this.$store.getters.settings.LimitQurey,
+        Sort: "+id",
+        Status: undefined,
+      },
     };
   },
   computed: {
     ...mapGetters(["Id", "name"]),
   },
   created() {
-    this.getdata();
+    this.handleFilter();
   },
   methods: {
     getdata() {
@@ -85,19 +175,25 @@ export default {
         Id: this.Id,
         name: this.name,
       };
-      this.loading = true;
-      GetDriverOrder({ id: this.user.Id, name: this.user.name })
+      this.listLoading = true;
+      GetDriverOrder({ id: this.user.Id, name: this.user.name, Page: this.listQuery.Page ,Any:this.listQuery.Any, limit:this.listQuery.limit, Sort:this.listQuery.Sort, Status:this.listQuery.Status})
         .then((response) => {
           // handle success
-          console.log("order data", response);
-          this.DeliveryData = response;
-          this.loading = false;
+          this.list = response.items;
+          this.Totals = response.Totals;
+          //this.DeliveryData = response;
+          this.listLoading = false;
         })
         .catch((error) => {
           // handle error
           console.log(error);
         });
     },
+    handleFilter() {
+      this.listQuery.Page = 1;
+      this.getdata();
+    },
+  
   },
 };
 </script>
@@ -105,25 +201,24 @@ export default {
 <style lang="scss" scoped>
 .panel-group {
   .card-panel-col {
-    margin-bottom: 10px;
+    margin-bottom: 5px;
   }
 
   .card-panel {
-    height: 270px;
+    height: 170px;
     cursor: pointer;
     font-size: 14px;
     position: relative;
     overflow: hidden;
-    color: #009432;
     background: #fff;
     border-color: rgba(0, 0, 0, 0.05);
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.3);
     padding-left: 15px;
     padding-right: 10px;
+    padding-top: 5px;
 
     &:hover {
       .card-panel-icon-wrapper {
-        color: #fff;
         background: #1b3459;
       }
     }
@@ -131,73 +226,72 @@ export default {
     .card-panel-icon-wrapper {
       color: #36a3f7;
       float: left;
-      margin: 14px 0 0 14px;
-      padding: 16px;
+      padding: 5px;
       transition: all 0.38s ease-out;
       border-radius: 6px;
     }
 
     .card-panel-icon {
       float: left;
-      font-size: 50px;
+      font-size: 30px;
     }
 
     .card-panel-description {
       float: right;
       font-weight: bold;
-      margin: 10px;
+      margin: 3px;
       margin-left: 0px;
 
       .card-panel-name {
-        line-height: 18px;
+        line-height: 20px;
         color: rgba(0, 0, 0, 0.8);
-        font-size: 16px;
-        margin-bottom: 12px;
+        font-size: 12px;
         text-align: right;
       }
       .card-panel-time {
-        line-height: 18px;
+        line-height: 20px;
         color: rgba(0, 0, 0, 0.45);
-        font-size: 16px;
-        margin-bottom: 12px;
+        font-size: 14px;
+        margin-bottom: 4px;
         text-align: right;
       }
       .card-panel-phone {
         text-align: left;
-        line-height: 18px;
+        line-height: 20px;
         color: rgba(0, 0, 0, 0.8);
-        font-size: 20px;
+        font-size: 12px;
       }
       .card-panel-id {
         text-align: left;
-        line-height: 18px;
+        line-height: 20px;
         color: rgb(0, 0, 0);
-        font-size: 24px;
-      }
-
-      .card-panel-num {
-        font-size: 20px;
+        font-size: 14px;
       }
     }
   }
 }
 
-// @media (max-width: 200px) {
-//   .card-panel-description {
-//     display: none;
-//   }
-
-//   .card-panel-icon-wrapper {
-//     float: none !important;
-//     width: 100%;
-//     height: 100%;
-//     margin: 0 !important;
-
-//     .svg-icon {
-//       display: block;
-//       margin: 14px auto !important;
-//       float: none !important;
-//     }
-//   }
-// }
+@media only screen and (max-width: 767px) {
+  .el-col-xs-24 {
+    .card-panel {
+      height: 190px;
+      padding-top: 5px;
+      .card-panel-description {
+        float: none;
+        .card-panel-id {
+          text-align: center;
+          line-height: 25px;
+        }
+        .card-panel-name {
+          text-align: center;
+          line-height: 25px;
+        }
+        .card-panel-time {
+          text-align: center;
+          line-height: 25px;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/views/OrderDelivery/components/OrderDetailsMobile.vue
+++ src/views/OrderDelivery/components/OrderDetailsMobile.vue
@@ -1,20 +1,20 @@
 <template>
 <div>
-           <el-col :span="12" v-if="Status == 0">
+           <el-col :span="12" v-if="Temp.Status == 0" style="padding-top: 10px;">
              <el-button 
           style="float: right"
           icon="el-icon-info"
           type="primary"
-          size="medium"
+          :size="$store.getters.size"
           @click="dialogFormVisible = true"
           >{{$t("Delivery.Details")}} </el-button>
           </el-col> 
-          <el-col :span="24" v-else>
+          <el-col :span="24" v-else >
              <el-button 
-          style="float: right"
+          style="float: right; "
           icon="el-icon-info"
           type="primary"
-          size="medium"
+          :size="$store.getters.size"
           @click="dialogFormVisible = true"
           >{{$t("Delivery.Details")}} </el-button>
           </el-col> 
@@ -31,28 +31,30 @@
         <i class="el-icon-user"></i>
         {{$t("Delivery.CustomerName")}}
       </template>
-      <el-tag size="small">{{Name}}</el-tag>
+      <el-tag size="small">{{Temp.Name}}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-location-outline"></i>
         {{$t("AddVendors.Region")}}
       </template>
-      <el-tag size="small">{{Region}}</el-tag>
+      <el-tag size="small">{{Temp.Region}}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-mobile-phone"></i>
          {{$t("Delivery.Phone")}}
       </template>
-      <el-tag size="small">{{PhoneNumber}}</el-tag>
+      <el-tag size="small" type="success">
+      <a :href="`tel:+${ Temp.PhoneNumber }`"> {{ Temp.PhoneNumber }} </a>
+      </el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-tickets"></i>
         {{$t("Items.Notes")}}
       </template>
-      <el-tag size="small">{{Description}}</el-tag>
+      <el-tag size="small">{{Temp.Description}}</el-tag>
     </el-descriptions-item>
   
   </el-descriptions>
@@ -63,49 +65,49 @@
         <i class="el-icon-star-off"></i>
          {{$t("Delivery.OrderNo")}}
       </template>
-      <el-tag size="small">{{Id}}</el-tag>
+      <el-tag size="small">{{Temp.Id}}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-date"></i>
         {{$t("Sales.Date")}}
       </template>
-      <el-tag size="small">{{ FakeDate | parseTime("{m}-{d} {h}:{i}") }}</el-tag>
+      <el-tag size="small">{{ Temp.FakeDate | parseTime("{m}-{d} {h}:{i}") }}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-s-flag"></i>
         {{$t("OrderInventories.Status")}}
       </template>
-      <el-tag size="small">{{Status}}</el-tag>
+      <el-tag size="small">{{Temp.Status}}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-money"></i>
         {{$t("Delivery.DeliveryPrice")}}
       </template>
-      <el-tag size="small">{{DeliveryPrice}}</el-tag>
+      <el-tag size="small">{{Temp.DeliveryPrice}}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-money"></i>
         {{$t("Delivery.ReceiptTotal")}}
       </template>
-      <el-tag size="small">{{TotalPill}}</el-tag>
+      <el-tag size="small">{{Temp.TotalPill}}</el-tag>
     </el-descriptions-item>
     <el-descriptions-item :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-money"></i>
          {{$t("Delivery.TotalAmmount")}} 
       </template>
-      <el-tag size="small">{{TotalPrice}}</el-tag>
+      <el-tag size="small">{{Temp.TotalPrice}}</el-tag>
     </el-descriptions-item>
-     <el-descriptions-item v-if="Status != 0" :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
+     <el-descriptions-item v-if="Temp.Status != 0" :label-style="{'text-align': 'right'}" :content-style="{'text-align': 'right'}">
       <template slot="label">
         <i class="el-icon-user-solid"></i>
          {{$t("Delivery.DriverName")}}
       </template>
-      <el-tag size="small">{{Driver.Name}}</el-tag>
+      <el-tag size="small">{{Temp.Driver.Name}}</el-tag>
     </el-descriptions-item>
      <!-- <el-descriptions-item>
       <template slot="label">
@@ -121,8 +123,8 @@
 </template>
 <script>
 export default {
-  name: 'OrderDetails',
- props: ["Id", "Name", "PhoneNumber","TotalPill","TotalPrice","FakeDate","Region", "Status", "Description", "DeliveryPrice", "Driver", "Content" ],
+  name: 'OrderDetailsMobile',
+ props: ["Temp" ],
   data () {
     
     return {
@@ -130,9 +132,6 @@ export default {
     }
  
   },
-     methods: {
-     
-    },
 }
 
 </script>
