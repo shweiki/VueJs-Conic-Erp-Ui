328948e0096902f5368882e99a978e69bb188afa
Taha Shweiki <tahashweiki@yahoo.com>
2022-04-07 16:18:27 +0300

    Update Member.vue

--- src/views/UploadData/Member.vue
+++ src/views/UploadData/Member.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="app-container">
-    <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
-    <el-button @click="AddMember" plain :disabled="isDisabled" type='success'>Push</el-button>
+    <upload-excel-component
+      :on-success="handleSuccess"
+      :before-upload="beforeUpload"
+    />
+    <el-button @click="AddMember" plain :disabled="isDisabled" type="success"
+      >Push</el-button
+    >
     <el-table
       height="250"
       v-loading="loading"
       :data="tableData"
       border
       highlight-current-row
-      style="width: 100%;margin-top:20px;"
+      style="width: 100%; margin-top: 20px"
     >
-      <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
+      <el-table-column
+        v-for="item of tableHeader"
+        :key="item"
+        :prop="item"
+        :label="item"
+      />
     </el-table>
   </div>
 </template>
@@ -28,7 +38,7 @@ export default {
       loading: false,
       tableData: [],
       data: [],
-      tableHeader: []
+      tableHeader: [],
     };
   },
   methods: {
@@ -37,26 +47,26 @@ export default {
       this.isDisabled = true;
 
       Create(this.data[0])
-        .then(response => {
+        .then((response) => {
           console.log("tag", "" + response);
           this.data.splice(0, 1);
           if (this.data.length != 0) {
             this.AddMember();
           } else {
-            this.loading = false
+            this.loading = false;
             this.tableData = [];
             this.$notify({
               title: "تم ",
               message: "تم الإضافة بنجاح",
-              type: 'success',
-              duration: 2000
-            })
+              type: "success",
+              duration: 2000,
+            });
           }
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
           //this.AddMember(100), 1000);
-        })
+        });
     },
 
     beforeUpload(file) {
@@ -68,33 +78,32 @@ export default {
 
       this.$message({
         message: "Please do not upload files larger than 8m in size.",
-        type: "warning"
-      })
+        type: "warning",
+      });
       return false;
     },
     handleSuccess({ results, header }) {
       this.loading = true;
       this.tableData = results;
       console.log(this.tableData);
-      this.data = this.tableData.map(element => {
+      this.data = this.tableData.map((element) => {
         return {
           Id: undefined,
-          Name:
-            element.n1 + " " + element.n2 + " " + element.n3 + " " + element.n4,
-          Ssn: element.ssn,
-          DateofBirth: this.ExcelDateToJSDate(element.bd),
-          Email: '',
-          PhoneNumber1: element.p1,
-          PhoneNumber2: element.p2,
-          Description: "From First Data",
+          Name: element.Name,
+          Ssn: element.Ssn,
+          DateofBirth: this.ExcelDateToJSDate(element.DateofBirth),
+          Email: "",
+          PhoneNumber1: element.PhoneNumber1,
+          PhoneNumber2: element.PhoneNumber2,
+          Description: element.Description,
           Type: "New",
           Status: 0,
-          Tag: element.tag
+          Tag: element.Tag,
         };
-      })
+      });
       this.tableHeader = header;
       this.isDisabled = false;
-      this.loading = false
+      this.loading = false;
     },
     ExcelDateToJSDate(serial) {
       var utc_days = Math.floor(serial - 25569);
@@ -120,7 +129,21 @@ export default {
         minutes,
         seconds
       );
-    }
-  }
+    },
+  },
 };
+/*       return {
+          Id: undefined,
+          Name:
+            element.n1 + " " + element.n2 + " " + element.n3 + " " + element.n4,
+          Ssn: element.ssn,
+          DateofBirth: this.ExcelDateToJSDate(element.bd),
+          Email: '',
+          PhoneNumber1: element.p1,
+          PhoneNumber2: element.p2,
+          Description: "From First Data",
+          Type: "New",
+          Status: 0,
+          Tag: element.tag
+        };*/
 </script>
