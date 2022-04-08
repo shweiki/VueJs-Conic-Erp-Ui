f515fd7e480ea2cd3ceb7c4a7f671e2dc5898861
Taha Shweiki <tahashweiki@yahoo.com>
2022-04-08 23:33:39 +0300

    fix payment close

--- src/views/CashPool/components/SaleInvoice.vue
+++ src/views/CashPool/components/SaleInvoice.vue
@@ -342,7 +342,7 @@ export default {
       tableData: [],
       CashPool: {},
       Data: undefined,
-      AutoSent: true,
+      AutoSent: false,
       CashAccountId: undefined,
       InComeAccountId: undefined,
       Totals: {
