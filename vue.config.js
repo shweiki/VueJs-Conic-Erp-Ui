f672016a2704551797cf14b1bbff5e5d9be581e3
Taha Shweiki <tahashweiki@yahoo.com>
2024-03-14 17:12:26 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const name =  'Conic Erp System' || defaultSettings.title // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.port || process.env.npm_config_port || 8080 // dev port
+const port = process.env.port || process.env.npm_config_port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -52,7 +52,7 @@ module.exports = {
   chainWebpack(config) {
     config.plugins.delete('preload') // TODO: need test
     config.plugins.delete('prefetch') // TODO: need test
-    
+
     // set svg-sprite-loader
     config.module
       .rule('svg')
